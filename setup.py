c30bdc98f3ec52ce8b74c00147e7d74a1b224585
Curt Micol <asenchi@asenchi.com>
2010-08-02 15:26:56 -0400

    Added packaging information and updated README.

---
+++ setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="proclaim",
+    version="0.3",
+    description="Conditionally roll out features with Redis",
+    long_description="""
+    Conditionally roll out features with Redis by assigning
+    percentages, groups or users to features. This is a port of jamesgolick's
+    rollout: http://github.com/jamesgolick/rollout""",
+    author="Curt Micol",
+    license="MIT",
+    author_email="asenchi@asenchi.com",
+    url="http://github.com/asenchi/proclaim",
+    download_url="http://github.com/asenchi/proclaim/downloads",
+    keywords="redis rollout",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Web Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independant",
+        "Topic :: Software Development"
+    ],
+    py_modules=["proclaim"],
+)
